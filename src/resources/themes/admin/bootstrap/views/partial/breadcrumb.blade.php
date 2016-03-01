ffa4b514ba87d4e573faaeac7f35f7907a5b6e94
bryan <brysalazar12@gmail.com>
2016-03-01 13:48:23 +0800

    separate admin theme

--- src/resources/themes/admin/bootstrap/views/partial/breadcrumb.blade.php
+++
@@ -1,3 +0,0 @@
-<ol class="breadcrumb">
-	@include('partial.bootstrap-breadcrumb-items', array('items' => Menu::get('breadcrumb')->roots()))
-</ol>
--- src/resources/themes/admin/bootstrap/views/partial/left-menu.blade.php
+++
@@ -1,6 +0,0 @@
-            <div class="collapse navbar-collapse navbar-ex1-collapse">
-					<!--{!! Menu::get('leftMenu')->asUl(['class'=>'nav navbar-nav side-nav']) !!}-->
-					<ul class="nav navbar-nav side-nav">
-					@include(config('laravel-menu.views.bootstrap-items'), array('items' => Menu::get('leftMenu')->roots()))
-					</ul>
-            </div>
